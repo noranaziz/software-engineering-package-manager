260044fac2c915108c6ecccd0a9ec9009a39c6f5
noranaziz <114042364+noranaziz@users.noreply.github.com>
2025-01-25 11:12:54 -0500

    Delete phase 2/phase2-main/MVP/src directory

--- phase 2/phase2-main/MVP/src/TestUtils/posttest.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This runs a function after tests complete, posttest runs automatically after npm run test
- * @author DSinc
- */
-import { showTestMetrics } from '../Processors/testProcessor';
-showTestMetrics();
